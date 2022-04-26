731cd603062014260bf8e71fafa33824fac20baa
Ana Zitzow <anasempai89@gmail.com>
2022-04-25 19:56:00 -0700

    commit tree 9d219678ea7e656d528120075851f7081221f484

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(num) {
   // Write your code here!
+  if (num > 2500) {
+    return 'No can do.';
+  }
+  if (num <= 400) {
+    return 'This one is on me!';
+  }
+  if (num >= 400 && num <= 2000) {
+    return 'That will be twenty bucks.';
+  }
+  if (num >= 2000) {
+    return 'I will gladly take your thirty bucks.';
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
   // Write your code here!
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
